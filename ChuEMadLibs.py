ba1f53010a2ced7696d480f1e887b75eb4d3ec81
ethanchu00 <58082520+ethanchu00@users.noreply.github.com>
2019-11-22 10:55:30 -0500

    Add files via upload

---
+++ ChuEMadLibs.py
@@ -0,0 +1,24 @@
+
+print("Ethan Chu Madlibs")
+print("Nihilsm in the future")
+print("Please insert your words below")
+
+print(" ")
+adjective = input("Type a feeling that life gives you: ")
+adjective2 = input("Type a feeling your existence makes you feel: ")
+number = input("What number is the meaning of life?: ")
+verb = input("What will robots do to us if they were conscious?: ")
+noun = input("What kind of living being are you not?: ")
+
+print(" ")
+print("Life to me, is very " + adjective)
+print("Thinking about exitence makes me very " + adjective2)
+print("The meaning of life from The Space Odessey is " + number)
+print("AI will " + verb + " humanity in the future")
+print(noun + " will be the future dominant ihabitants of Earth")
+print("To prevent this outcome, I must travel back in time and eliminate the " 
+      + noun + " before they become self aware")
+
+print(" ")
+print("Thank you for your input, your answers will be utilized for understanding humankind, for a future genocide of homo sapiens")
+print("You have " + number + " days left to exist")
